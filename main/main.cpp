8ca9a8f0bd8570a71ef918b3a20895dceca21484
João Soares <ojoaosoaressantos@gmail.com>
2023-11-02 21:53:10 -0300

    feat and refactor: função que salva os dados dos filmes em um repositorio backup, e também a criação de constantes para cada comando possível e mudanças de nome de varíavel para deixar seu objetivo mais evidente

--- main/main.cpp
+++ main/main.cpp
@@ -4,6 +4,18 @@
 #include <vector>
 #include <iterator>
 
+#define LER_ARQUIVO "LA"
+#define CADASTRAR_FILME "CA"
+#define REMOVER_FILME "RF"
+#define CADASTRAR_CLIENTE "CC"
+#define LISTAR_FILMES "LF"
+#define REMOVER_ClIENTES "RC"
+#define LISTAR_CLIENTES "LC"
+#define ALUGAR_FILME "AL"
+#define DEVOLVER_FILME "DV"
+#define FINALIZAR_SISTEMA "FS"
+
+
 using namespace std;
 
 // classe feita apenas para fins de teste
@@ -36,34 +48,36 @@ void removerEspacosDireitaEsquerda(string &linha)  {
     linha.erase(linha.find_last_not_of(' ')+1); // remove os espaços a esquerda, a função find_last_not_of retorna o ultimo caracter que não é um espaço
 }
 
-void lerArquivo(string file, vector<Filme> &filmes) {
+void lerArquivo(vector<Filme> &filmes) {
 
+    string nome_arquivo;
+    cin >> nome_arquivo;
 
-    ifstream movies(file, ios::in);
+    ifstream arquivo_filmes(nome_arquivo, ios::in);
     // O Arquivo é aberto em modo de leitura
 
-    if(!movies.is_open()) {
+    if(!arquivo_filmes.is_open()) {
         cout << "Erro: arquivos inexistente" << endl;
         return;
         // Caso o arquivo não exista, uma mensagem de erro é exibida e a função é encerrada
     }
 
 
-    char tipo;
-    int quantidade, codigo, total = 0;
+    char tipo_item;
+    int quantidade_cada_filme, codigo_item, total_filmes_unicos = 0;
     vector <string> categorias_dvd = {"Lançamento", "Promoção", "Estoque"};
-    string nome, categoria_dvd, linha;
+    string titulo_item, categoria_dvd, linha;
     
 
-    while (movies >> tipo >> quantidade >> codigo)
+    while (arquivo_filmes >> tipo_item >> quantidade_cada_filme >> codigo_item)
     {
-        total++;
+        total_filmes_unicos++;
         // Os valores tipo(char), quantidade (int) e codigo(int) são lido separadamente
-        getline(movies,linha);
+        getline(arquivo_filmes,linha);
         // Como não sabemos se o nome do filme tem mais de uma palavra, faremos uma analise posteriormente para separar a categoria do nome
         
 
-        if(tipo == 'D') {
+        if(tipo_item == 'D') {
             // Caso o tipo de mídia seja Dvd, então ele terá obrigatóriamente uma categoria
             for (string categoria : categorias_dvd) // O foreach procurará na linha todos os tipos de categoria
             {
@@ -72,7 +86,7 @@ void lerArquivo(string file, vector<Filme> &filmes) {
                     categoria_dvd = categoria;
                     linha.erase(pos,categoria.length()); // a função erase remove a substring da linha
                     removerEspacosDireitaEsquerda(linha);
-                    nome = linha; 
+                    titulo_item = linha; 
                     // Assim o restante da linha será o nome do filme
                     break;
                 }
@@ -86,41 +100,48 @@ void lerArquivo(string file, vector<Filme> &filmes) {
         else {
             // Caso a mídia seja do tipo fita, então ela não terá categoria, e então a propria linha sera o nome do filme
             categoria_dvd = "None";
-            nome = linha;
+            titulo_item = linha;
         }
 
-        Filme filme(tipo,quantidade,codigo,nome,categoria_dvd);
+        Filme filme(tipo_item,quantidade_cada_filme,codigo_item,titulo_item,categoria_dvd);
         filmes.push_back(filme);
         // Uma nova instancia de filme é criada e armazenada
         
     } 
-    cout << total << " Filmes cadastrados com sucesso" << endl;
-    movies.close();
+    cout << total_filmes_unicos << " Filmes cadastrados com sucesso" << endl;
+    arquivo_filmes.close();
 }
 
+void salvarDados(vector<Filme> &filmes) {
+    ofstream arquivo_filmes("../backups/filmes_backup",ios::out | ios::trunc);
 
+    if(!arquivo_filmes.is_open()) {
+        cout << "Erro: não foi possível criar o arquivo" << endl;
+        return;
+        // Caso o arquivo não seja criado, uma mensagem de erro é exibida e a função é encerrada
+    }
+
+    for(Filme &filme : filmes) 
+        arquivo_filmes << filme.tipo  << " " << filme.codigo << " " << filme.quantidade << 
+        " " << filme.codigo << " " << filme.nome << " " << filme.dvd_tipo << endl;
+
+    arquivo_filmes.close();
+
+}
 
 
 
 
 int main() {
 
-
-    string filename, comando;
+    string comando;
     vector<Filme> filmes;
     
     while (true)
     {
-
         cin >> comando;
         
-        if(comando == "LA") {
-
-            cin >> filename;
-            lerArquivo(filename,filmes);
-
-            
-        }
+        if(comando == LER_ARQUIVO) lerArquivo(filmes);
 
         else if(comando == "LS") {
             // comando teste 
@@ -131,7 +152,11 @@ int main() {
 
         }
 
-        else if(comando == "FS") break;
+        else if(comando == FINALIZAR_SISTEMA) {
+            salvarDados(filmes);
+            break;
+        }
+            
         
         
     }
