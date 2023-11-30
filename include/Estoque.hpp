aae599590c6bba2f31638d59e0e1a6042a6788ba
unknown <arifilhosilva235@gmail.com>
2023-11-30 09:40:15 -0300

    docs: adiciona comentários da classe Estoque

--- include/Estoque.hpp
+++ include/Estoque.hpp
@@ -6,6 +6,11 @@
 #include <chrono>
 #include <functional>
 
+/**
+ * @file Estoque.hpp
+ * @brief Definição da classe Estoque, responsável por gerenciar o estoque de filmes.
+ */
+
 const std::string DIRETORIO_PADRAO_FILMES = "./data/Filmes/filmes";
 const std::string DIRETORIO_LOG_FILMES = "./data/Filmes/logs";
 const std::string DIRETORIO_HISTORICO_FILMES = "./data/Filmes/Historico/";
@@ -18,6 +23,11 @@ const std::map<std::string, std::function<bool(const Filme *, const Filme *)>> C
     {"U", [](const Filme *a, const Filme *b)
      { return a->getUnidades() < b->getUnidades(); }}};
 
+/**
+ * @class Estoque
+ * @brief Classe para gerenciar o estoque de filmes.
+ */
+
 class Estoque
 {
 private:
@@ -26,14 +36,62 @@ private:
 public:
     Estoque();
     ~Estoque();
+
+    /**
+     * @brief Lê os filmes em estoque de um arquivo.
+     * @param diretorio O caminho do arquivo a ser lido.
+     */
     void lerArquivo(const std::string diretorio);
+
+    /**
+     * @brief Insere um novo filme no estoque.
+     * @param novoFilme Ponteiro para o novo filme a ser inserido.
+     * @return true se o filme foi inserido com sucesso, false caso contrário.
+     */
     bool inserirFilme(Filme *novoFilme);
+
+    /**
+     * @brief Remove um filme do estoque.
+     * @param identificador O identificador do filme a ser removido.
+     */
     void removerFilme(const int identificador);
+
+    /**
+     * @brief Pesquisa filmes pelo identificador.
+     * @param identificador O identificador a ser pesquisado.
+     */
     void pesquisarFilmesCodigo(const int identificador) const;
+
+    /**
+     * @brief Pesquisa filmes pelo título.
+     * @param titulo O título a ser pesquisado.
+     */
     void pesquisarFilmesTitulo(const std::string titulo) const;
+
+    /**
+     * @brief Lista os filmes ordenados por critério.
+     * @param ordenacao O critério de ordenação (C - Código, N - Nome, U - Unidades).
+     */
     void listarFilmesOrdenados(const std::string ordenacao) const;
+
+    /**
+     * @brief Verifica se um filme com determinado identificador existe no estoque.
+     * @param identificador O identificador a ser verificado.
+     * @return Ponteiro para o filme se existir, nullptr caso contrário.
+     */
     Filme *filmeExiste(const int identificador) const;
+
+    /**
+     * @brief Verifica se um filme com determinado identificador é válido para locação.
+     * @param identificador O identificador a ser verificado.
+     * @return Ponteiro para o filme se válido, nullptr caso contrário.
+     */
     Filme *filmeValido(const int identificador) const;
+
+    /**
+     * @brief Salva os filmes do estoque em um arquivo.
+     * @param limparDados Indica se os filmes do estoque devem ser desalocados após a escrita.
+     */
     void salvarDados(const bool limparDados);
 };
 
