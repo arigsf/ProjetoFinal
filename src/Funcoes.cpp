43d893f87526073c43e86d4afb236e297682feb1
João Soares <ojoaosoaressantos@gmail.com>
2023-12-01 14:55:49 -0300

    feat: Implementei a função toUpperCase na classe Sistema e no main.

--- src/Funcoes.cpp
+++ src/Funcoes.cpp
@@ -71,3 +71,10 @@ bool isNumeroValido(const std::string& s)
     return !s.empty() && std::find_if(s.begin(), 
         s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
 }
+
+
+std::string toUpperCase(const std::string& s) {
+    std::string copia = s;
+    std::transform(copia.begin(), copia.end(), copia.begin(), ::toupper);
+    return copia;
+}
